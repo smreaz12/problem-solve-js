3758e222e80e025dfec289ded947cbf33e1091b2
smreaz12 <smreaz555@gmail.com>
2026-01-30 23:16:23 +0600

    solve-2

--- problem1.js
+++ problem1.js
@@ -1,7 +1,23 @@
+//IEEE 754 Floating-Point Precision Error//
+//Binary Floating-Point Representation Issue//
+
+
+//solve-1//
 let a = 0.2;
 let b = 0.1;
-console.log(a+b)
+console.log(a+b);
 
 let x = a+b;
 x=Number(x.toFixed(2));
 console.log(x===0.3);
+
+
+//solve-2//
+let c = 0.2;
+let d = 0.1;
+console.log(c+d);
+
+let y = c+d;
+console.log(Math.abs(y - 0.3) < Number.EPSILON); 
+
+
