6618f6f7032d2e6eb64d5cd1b47c4e4138ad2d23
smreaz12 <smreaz555@gmail.com>
2026-02-02 17:14:24 +0600

    convert mile to kilometer

---
+++ convert.js
@@ -0,0 +1,17 @@
+//mile to kilometer//
+
+function mileToKilo(mile){
+    const kilo = mile*1.60934;
+    return kilo;
+}
+
+const result = mileToKilo(5);
+console.log(result);
+
+//
+function mileToKilo(mile){
+    const kilo = mile * 1.60934;
+    return kilo;
+}
+
+console.log(mileToKilo(5));
