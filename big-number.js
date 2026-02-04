ebb14d78272a30423a102bd4d84c4d34d64c00d4
smreaz12 <smreaz555@gmail.com>
2026-02-04 22:48:41 +0600

    get big number in array

---
+++ big-number.js
@@ -0,0 +1,10 @@
+let numbers = [10, 25, 7, 40, 15];
+let max = numbers[0];
+
+for (let i = 1; i < numbers.length; i++) {
+  if (numbers[i] > max) {
+    max = numbers[i];
+  }
+}
+
+console.log(max);
